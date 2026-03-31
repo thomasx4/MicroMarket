5cad7414ea2bd4f6a95a373e55a7f85b71e808ad
krisgallego <krisgallego7@gmail.com>
2026-03-31 15:19:28 -0500

    KM: feat: categories.java entity

--- src/main/java/com/gestion/micromarket/entity/Products.java
+++ src/main/java/com/gestion/micromarket/entity/Products.java
@@ -1,5 +1,43 @@
 package com.gestion.micromarket.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
 public class Products {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String barcode;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Long stock;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Categories category;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
