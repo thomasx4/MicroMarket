e0859cc7ee58c96874704350ff33bb19b645e34f
krisgallego <krisgallego7@gmail.com>
2026-03-31 18:45:17 -0500

    KM: feat: Creating MessageResponseDTO.java dto and content

---
+++ src/main/java/com/gestion/micromarket/dto/MessageResponseDTO.java
@@ -0,0 +1,13 @@
+package com.gestion.micromarket.dto;
+
+public class MessageResponseDTO {
+    private String message;
+
+    public MessageResponseDTO(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/com/gestion/micromarket/repository/CategoriesRepository.java
+++ src/main/java/com/gestion/micromarket/repository/CategoriesRepository.java
@@ -3,6 +3,10 @@ package com.gestion.micromarket.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.gestion.micromarket.entity.Categories;
 
+import java.util.Optional;
+
 public interface CategoriesRepository extends JpaRepository<Categories, Long> {
-    
+
+    Optional<Categories> findByName(String name);
+
 }
