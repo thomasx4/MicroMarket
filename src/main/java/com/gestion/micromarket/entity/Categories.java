f0f029215c156578a3393e16f49340ed756a02d4
krisgallego <krisgallego7@gmail.com>
2026-03-31 20:01:22 -0500

    KM: feat: Categories.java Entity

--- src/main/java/com/gestion/micromarket/entity/Categories.java
+++ src/main/java/com/gestion/micromarket/entity/Categories.java
@@ -19,5 +19,10 @@ public class Categories {
 
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
     
 }
